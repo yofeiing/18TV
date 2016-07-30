ef5577f2d4872ebc2e4775a6c2d42e0a6f7df0c3
fuyifang <919161460@qq.com>
2016-07-30 19:52:49 +0800

    something change

--- app/src/main/java/com/example/fuyifang/androidtv/adapter/InfoAdapter.java
+++ app/src/main/java/com/example/fuyifang/androidtv/adapter/InfoAdapter.java
@@ -3,21 +3,24 @@ package com.example.fuyifang.androidtv.adapter;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.fuyifang.androidtv.R;
+import com.example.fuyifang.androidtv.bean.InfoBean;
 import com.example.fuyifang.androidtv.bean.TodayRecommende;
+import com.example.fuyifang.androidtv.utils.LogUtil;
 
 import java.util.List;
 
 /**
  * Created by fuyifang on 2016/7/26.
  */
-public class InfoAdapter extends BaseQuickAdapter<TodayRecommende> {
-    public InfoAdapter(int layoutResId, List<TodayRecommende> data) {
+public class InfoAdapter extends BaseQuickAdapter<InfoBean> {
+    public InfoAdapter(int layoutResId, List<InfoBean> data) {
         super(layoutResId, data);
+//        LogUtil.i(data.get(0).getRecommend().get(0).getName());
     }
 
     @Override
-    protected void convert(BaseViewHolder baseViewHolder, TodayRecommende todayRecommende) {
-        baseViewHolder.setImageResource(R.id.recommon_img,todayRecommende.getRecommendImg());
-        baseViewHolder.setText(R.id.recommon_name,todayRecommende.getRecommendName());
+    protected void convert(BaseViewHolder baseViewHolder, InfoBean infobean) {
+        baseViewHolder.setText(R.id.recommon_name,infobean.getRecommend().get(0).getName());
+
     }
 }
--- app/src/main/java/com/example/fuyifang/androidtv/utils/LogUtil.java
+++ app/src/main/java/com/example/fuyifang/androidtv/utils/LogUtil.java
@@ -10,8 +10,8 @@ public class LogUtil {
     {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
-    public static boolean isDebug = true;// 是否需要打印bug，可以在application的onCreate函数里面初始化
-    private static final String TAG = "way";
+    public static boolean isDebug = true;
+    private static final String TAG = "Tag";
 
     // 下面四个是默认tag的函数
     public static void i(String msg)
